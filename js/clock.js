7779ca1b65a8d2bc19fcb5cdcb8dd4ba5ca52896
sinan akyol <sinanakyol01@gmail.com>
2022-09-06 16:28:50 +0300

    files changed

--- js/clock.js
+++ js/clock.js
@@ -5,14 +5,23 @@ myName.innerHTML = prompt('Lütfen adınızı giriniz:')
 
 function showTime(){
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
     const today = new Date();
-    let day = weekday[today.getUTCDay()];
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.querySelector('#myClock').innerHTML = h + ":" + m + ":" + s + " " + day;
+    let curday = weekday[today.getDay()];
+    let hour = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+    min = checkTime(min);
+    sec = checkTime(sec);
+    
+    let day = today.getDate();
+    let month = months[today.getMonth()];
+    let year = today.getFullYear();
+    
+    let date = hour + ":" + min + ":" + sec + " " + curday+","+" "+day+" "+month+" "+year;
+
+    document.querySelector('#myClock').innerHTML = date;
     setTimeout(showTime, 1000);
 
 }
@@ -20,9 +29,6 @@ function showTime(){
 function checkTime(i){
     if (i < 10) {i = "0" + i};
     return i;
-
 }
+
 showTime()
-
-
-
